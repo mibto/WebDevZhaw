fc569ffddcedda0a4e11e732df47ac80ab095bf0
Michael Brodmann <mib@doodle.com>
2014-05-27 15:59:19 +0200

    Kapitel 10

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,18 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
   # GET /carts.json
   def index
     @carts = Cart.all
   end
+
+  private
+  def invalid_cart
+logger.error "Attempt to access invalid cart #{params[:id]}"
+redirect_to store_url, notice: 'Invalid cart'
+end
 
   # GET /carts/1
   # GET /carts/1.json
@@ -54,10 +61,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
-    respond_to do |format|
-      format.html { redirect_to carts_url }
-      format.json { head :no_content }
+    @cart.destroy if @cart.id == session[:cart_id]
+session[:cart_id] = nil
+respond_to do |format|
+format.html { redirect_to store_url,
+notice: 'Your cart is currently empty' }
+format.json { head :no_content }
     end
   end
 
